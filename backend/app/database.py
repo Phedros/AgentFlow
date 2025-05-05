d610993c806f486a4143f389fa5c881ed75fdd1c
Phedros <pedroserra86@gmail.com>
2025-05-04 21:11:39 -0300

    add conection to DB

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,16 @@
-#project-root/backend/app/database.py
+# project-root/backend/app/database.py
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # carga .env en variables de entorno
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("🛑 ERROR: no se encontró DATABASE_URL. Definíla en .env o en el entorno.")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
